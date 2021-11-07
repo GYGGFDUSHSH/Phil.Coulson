a75e656efa712d8345663682074d58ceac9bcd34
xxxᴛᴇɴᴛᴀᴄɪᴏɴ <81632023+xxxtentacion-TG@users.noreply.github.com>
2021-11-07 15:31:25 +0530

    Update info.py

--- info.py
+++ info.py
@@ -12,18 +12,29 @@ BOT_TOKEN = "1651274124:AAGSuYUJTzvmuNA42U8qNfutJUtBjquWd4U"
 
 # Bot settings
 CACHE_TIME = "300"
-USE_CAPTION_FILTER = False
-PICS = "https://telegra.ph/file/d32169a517600a90812a9.jpg"        
+USE_CAPTION_FILTER = False        
+PICS = [
+  "https://telegra.ph/file/3002281196303098ed719.jpg",
+  "https://telegra.ph/file/ba325e42787cc6cfb8f9c.jpg",
+  "https://telegra.ph/file/d7483ff134a647606bdbd.jpg",
+  "https://telegra.ph/file/fd34364fd60b16dba86e5.jpg",
+  "https://telegra.ph/file/1eddd2e17d3c433329644.jpg",
+  "https://telegra.ph/file/4d710f5ede210a8d33a9b.jpg",
+  "https://telegra.ph/file/126c3d59725f22317b231.jpg",
+  "https://telegra.ph/file/223f3f92abb1fa6e95c7d.jpg",
+  "https://telegra.ph/file/18ada04c1a18f234fb946.jpg",
+  "https://telegra.ph/file/c39c9017ce59d1d201b17.jpg",
+  "https://telegra.ph/file/7dbb3f0cbd1491061b0a7.jpg",
+  "https://telegra.ph/file/11d9370e562d72cbffc99.jpg",
 
 # Admins, Channels & Users
-ADMINS = [1560645285]
-CHANNELS = [-1001462830769]
-AUTH_USERS = ADMINS
+ADMINS = [1103647866]
+CHANNELS = [-1001500220248]
+AUTH_USERS = [1560645285]
 AUTH_CHANNEL = None
-AUTH_GROUPS = -1001277599754
 
 # MongoDB information
-DATABASE_URI = "mongodb+srv://Wafikh:wafikh@cluster0.sjzmu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+DATABASE_URI = "mongodb+srv://evamaria2:evamaria2@cluster0.tx3ho.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 DATABASE_NAME = "Cluster0"
 COLLECTION_NAME = "Telegram_files"
 
