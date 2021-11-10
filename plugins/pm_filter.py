c375fdfb5912a855be5b8996d1679c5c218a5675
AaGath <89765070+AaGath@users.noreply.github.com>
2021-11-10 14:44:29 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -387,8 +387,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         await client.send_cached_media(
             chat_id=query.from_user.id,
             file_id=file_id,
-            caption=f_caption
-            reply_markup=InlineKeyboardMarkup(filebuttons)
+            caption=f_caption, 
             )
 
     elif query.data == "pages":
