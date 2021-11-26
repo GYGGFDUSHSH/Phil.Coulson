0ffc92b370e4a236ba393bba60413d2fb4ca414c
Don_Sflix <92365864+GYGGFDUSHSH@users.noreply.github.com>
2021-11-26 13:30:24 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,27 +1,29 @@
 class script(object):
     START_TXT = """𝙷𝙴𝙻𝙾 {},
-𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href='https://t.me/Wireless_TG'>𝙱𝚒𝚕𝚕𝚎 𝙴𝚒𝚕𝚒𝚜𝚑</a>, 𝙸 𝙲𝙰𝙽 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝙼𝙾𝚅𝙸𝙴𝚂, 𝙹𝚄𝚂𝚃 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 𝙰𝙽𝙳 𝙴𝙽𝙹𝙾𝚈 😍"""
+𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href='https://t.me/Phil_Coulson_Sflix_bot'>Phil Coulson</a>, 𝙸 𝙲𝙰𝙽 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝙼𝙾𝚅𝙸𝙴𝚂, 𝙹𝚄𝚂𝚃 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 𝙰𝙽𝙳 𝙴𝙽𝙹𝙾𝚈 😍"""
     HELP_TXT = """𝙷𝙴𝚈 {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
-    ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: 𝙱𝚒𝚕𝚕𝚒𝚎 𝙴𝚒𝚕𝚒𝚜𝚑
-✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: 𝚠𝚒𝚛𝚎𝚕𝚎𝚜𝚜
+    ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: Phil Coulson
+✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href='https://t.me/Don_Sflix/'>Don Sflix</a>
+✯ Our Channel : <a href='https://t.me/Sflix2k/'>Sflix</a>
+✯ Support Group : <a href='https://t.me/Sflix2kchats/'>Sflix Chats</a>
 ✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
 ✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
 ✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
 ✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄
 ✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
     SOURCE_TXT = """<b>NOTE:</b>
-- 𝙱𝚒𝚕𝚕𝚒𝚎 𝙴𝚒𝚕𝚒𝚜𝚑 𝙽𝚘𝚝 𝙰 𝙾𝚙𝚎𝚗 𝚂𝚘𝚞𝚛𝚌𝚎
+- Phil Coulson 𝙽𝚘𝚝 𝙰 𝙾𝚙𝚎𝚗 𝚂𝚘𝚞𝚛𝚌𝚎 Project
 -  
 
 <b>DEVS:</b>
-- <a href=https://t.me/Wireless_TG>𝙱𝚒𝚕𝚕𝚒𝚎 𝙴𝚒𝚕𝚒𝚜𝚑</a>"""
+- <a href=https://t.me/Don_Sflix>Phil Coulson</a>"""
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 
 - Filter is the feature were users can set automated replies for a particular keyword and tessa will respond whenever a keyword is found the message
 
 <b>NOTE:</b>
-1. eva maria should have admin privillage.
+1. Phil Coulson should have admin privillage.
 2. only admins can add filters in a chat.
 3. alert buttons have a limit of 64 characters.
 
@@ -32,11 +34,11 @@ class script(object):
 • /delall - <code>delete the whole filters in a chat (chat owner only)</code>"""
     BUTTON_TXT = """Help: <b>Buttons</b>
 
-- Eva Maria Supports both url and alert inline buttons.
+- Phil Coulson Supports both url and alert inline buttons.
 
 <b>NOTE:</b>
 1. Telegram will not allows you to send buttons without any content, so content is mandatory.
-2. Eva Maria supports buttons with any telegram media type.
+2. Phil Coulson supports buttons with any telegram media type.
 3. Buttons should be properly parsed as markdown format
 
 <b>URL buttons:</b>
